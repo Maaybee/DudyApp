a015ce59d067f186d85b209fb163fd14d337a149
Maaybe <112830614+Maaybee@users.noreply.github.com>
2025-09-09 11:07:13 -0300

    Update telaLoginKid.js

--- scripts/telaLoginKid.js
+++ scripts/telaLoginKid.js
@@ -69,7 +69,12 @@ function cadastrar_redirecionamento() {
 }
 
 function abrirMenu() {
-  document.getElementById("menu").style.display = "block";
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "none") { 
+    menu.style.display = "block";
+  } else { 
+    menu.style.display = "none";
+  }
 }
 
 function fecharMenu() {
