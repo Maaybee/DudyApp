c314646ef3401f7b3a70240fef4c434f01dbb42f
Maria Eduarda <eduarda.paulaif@gmail.com>
2025-09-15 02:32:33 -0300

    kl

--- scripts/scriptAtividade.js
+++ scripts/scriptAtividade.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- INICIALIZAÇÃO DO SUPABASE ---
-    const SUPABASE_URL = "SEU_SUPABASE_URL_AQUI";
-    const SUPABASE_ANON_KEY = "SUA_SUPABASE_ANON_KEY_AQUI";
+    const SUPABASE_URL = "https://jqteyocpfokvjmsrdiea.supabase.co";
+    const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdGV5b2NwZm9rdmptc3JkaWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MTQwNDIsImV4cCI6MjA3MTE5MDA0Mn0.SNBHJgmoXVIAx6d5uDIBU2OYfzIzyZMbqcigAuoSBtA";
     const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     const urlParams = new URLSearchParams(window.location.search);
