0f9bf2c8ad4901f997db3d329f3e0dffcd17430b
Maaybe <112830614+Maaybee@users.noreply.github.com>
2025-09-15 08:42:08 -0300

    Update telaLogin.js

--- scripts/telaLogin.js
+++ scripts/telaLogin.js
@@ -1,9 +1,3 @@
-// Arquivo: scripts/telaLogin.js
-
-// --- INICIALIZAÇÃO DO SUPABASE ---
-const SUPABASE_URL = "https://jqteyocpfokvjmsrdiea.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdGV5b2NwZm9rdmptc3JkaWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MTQwNDIsImV4cCI6MjA3MTE5OTk5OX0.SNBHJgmoXVIAx6d5uDIBU2OYfzIzyZMbC_igAuoSBtA"; // CHAVE ANON ATUALIZADA (EXEMPLO)
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.addEventListener("DOMContentLoaded", () => {
     // --- ELEMENTOS DA PÁGINA ---
