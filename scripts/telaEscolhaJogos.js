9247e31de60bb768c848363c24e548831f452920
Maaybe <112830614+Maaybee@users.noreply.github.com>
2025-10-07 12:16:54 -0300

    Update telaEscolhaJogos.js

--- scripts/telaEscolhaJogos.js
+++ scripts/telaEscolhaJogos.js
@@ -3,3 +3,9 @@ const sairBtnPrincipal = document.getElementById('sair');
 sairBtnPrincipal.addEventListener('click', () => {
     window.location.href = '../telas/telaHome.html';
 });
+
+const jogoMemoria = document.getElementById('jogo-memoria') 
+
+jogoMemoria.addEventListener('click', () => { 
+    window.location.href = 'telaJogo_memoria.html';
+});
