f5447775309265bc4b13318dacff81013d4d00aa
Maaybe <112830614+Maaybee@users.noreply.github.com>
2025-10-30 17:49:16 -0300

    Create App component with greeting message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, DudyApp!</h1>
+      <p>A compilação está funcionando.</p>
+    </div>
+  );
+}
+
+export default App;
